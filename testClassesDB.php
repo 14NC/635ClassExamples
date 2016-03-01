a51b74b5c56cc986c1f3989fd8853b11a1869219
DJ Kehoe <kehoed@njit.edu>
2016-02-29 20:21:28 -0500

    added PHP CLI sample

--- testClassesDB.php
+++ testClassesDB.php
@@ -4,21 +4,86 @@
 require_once("classdb.php.inc");
 require_once("logindb.php.inc");
 
-$loginDB = new loginDB("logindb.ini");
-
-echo "does user aqib exist?".PHP_EOL;
-echo $loginDB->checkIfUserExists("aqib").PHP_EOL;
-
-echo "addin new use notaqib!".PHP_EOL;
-$loginDB->addNewUser("notaqib","password");
-
-echo "validating user aqib with password 'password':".PHP_EOL;
-if ($loginDB->validateUser("notaqib","password"))
+function print_help()
 {
-    echo "validation successful".PHP_EOL;
+  echo "usage: ".PHP_EOL;
+  echo __FILE__." -u <username> -p <password> -c <command arguments...>".PHP_EOL;
 }
-else
+
+if ($argc < 2)
 {
-    echo "validation rejected!".PHP_EOL;
+   print_help();
+   exit(0);
 }
+$cArgs = array();
+for ($i = 0; $i < $argc;$i++)
+{
+  if (($argv[$i] === "-h") ||
+      ($argv[$i] === "--help"))
+  {
+      print_help();
+      exit(0);
+  }
+  if ($argv[$i] === '-u')
+  {
+    $username = $argv[$i + 1];
+    $i++;
+    continue;
+  }
+  if ($argv[$i] === '-p')
+  {
+    $password = $argv[$i + 1];
+    $i++;
+    continue;
+  }
+  if ($argv[$i] === '-c')
+  {
+    $command = $argv[$i + 1];
+    $i++;
+    continue;
+  }
+  $cArgs[] = $argv[$i];  
+}
+
+if (!isset($username))
+{
+   echo "no username specified".PHP_EOL;
+   print_help();
+   exit(0);
+}
+
+if (!isset($password))
+{
+   echo "no password specified".PHP_EOL;
+   print_help();
+   exit(0);
+}
+
+if (!isset($command))
+{
+   echo "no command specified".PHP_EOL;
+   print_help();
+   exit(0);
+}
+
+switch ($command)
+{
+  case 'updatePW':
+   $login = new loginDB("logindb.ini");
+   $login->updateUserPassword($username,$password,$cArgs[0]);
+   break;
+  case 'validatePW':
+   $login = new loginDB("logindb.ini");
+   if ($login->validateUser($username,$password))
+   {
+      echo "password validated!!!".PHP_EOL;
+   }
+   else
+   {
+      echo "password does not validate!".PHP_EOL;
+   }
+
+   break;
+}
+
 ?>
